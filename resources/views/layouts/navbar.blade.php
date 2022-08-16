1b7e6daf49879c80b2e008c432346322f43b46ce
Faisalramdani <muhamadfaisalramdani724@gmail.com>
2022-08-16 18:46:16 +0700

    Commit Merdeka

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -37,9 +37,6 @@
               <a href="{{ '/' }}" class="nav-link">Beranda</a>
           @endif
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="">Epaper</a>
-        </li>
         @if (Auth::user())
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
