d0193f4ef3333110555683cd435b10237b0e52df
Abdul aziz <55531723+dulaziz@users.noreply.github.com>
2022-08-24 14:35:57 +0700

    Update GoogleController.php

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -16,7 +16,7 @@ class GoogleController extends Controller
 
     public function handleGoogleCallback(){
         try{
-            $googleUser = Socialite::driver('google')->user();
+            $googleUser = Socialite::driver('google')->stateless()->user();
 
             // dd($googleUser);
 
