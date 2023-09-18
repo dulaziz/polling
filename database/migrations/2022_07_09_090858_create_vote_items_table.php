94f7a2fbf58682b7513c797714b6ba28e4889eb0
Kriti M <macbookpro@macbooks-MBP.lan>
2023-09-18 10:13:35 +0700

    add column premium profile on vote item

--- database/migrations/2022_07_09_090858_create_vote_items_table.php
+++ database/migrations/2022_07_09_090858_create_vote_items_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->string('slug')->nullable();
             $table->string('vote_position')->nullable();
             $table->text('description')->nullable();
+            $table->integer('premium_profile')->default(0);
             $table->timestamps();
         });
     }
